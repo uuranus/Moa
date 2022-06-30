8aaa570d19cead904ca9ca7de514c392ccea9989
uuranus <hyur0920@gmail.com>
2022-06-30 21:36:32 +0900

    home calendar&list

--- app/src/main/java/com/moa/moa/Home/HomeFragment.kt
+++ app/src/main/java/com/moa/moa/Home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.sundeepk.compactcalendarview.CompactCalendarView
@@ -55,6 +56,13 @@ class HomeFragment : Fragment() {
         getWorkInfo() //모든 집안일 정보 가져와서 workInfos에 저장
     }
 
+    private fun initWorkList(){
+        workList[0].list[0].list= emptyList()
+        for(section in workList[1].list){
+            section.list= emptyList()
+        }
+    }
+
     private fun init(){
 
         calenderMonthTextView.text=(dateFormatForMonth.format(calendarView.firstDayOfCurrentMonth))
@@ -63,8 +71,6 @@ class HomeFragment : Fragment() {
         calendarView.setListener(object:CompactCalendarView.CompactCalendarViewListener{
             override fun onDayClick(dateClicked: Date?) {
                 val events=calendarView.getEvents(dateClicked)
-
-                Log.i("events",workInfos.toString())
 
                 //집안일 목록 초기화
                 workList[0].list[0].list= mutableListOf()
@@ -89,86 +95,97 @@ class HomeFragment : Fragment() {
 
     private fun getMonthWork(year:String,month:String){
 
-        firebaseDatabase.child(groupId).child("log").child(year).child(month).get().addOnCompleteListener {
-            if(it.isSuccessful){
+        firebaseDatabase.child(groupId).child("log").child(year).child(month).addListenerForSingleValueEvent(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
                 //선택된 월의 데이터를 가져오기
                 //캘린더에 데이터를 넣어주기
 
-                    val format=SimpleDateFormat("yyyy-mm-dd")
-                  Log.i("date",format.format(format.parse("2022-07-22"))+groupId)
+                val format=SimpleDateFormat("yyyy-mm-dd")
+                Log.i("date",format.format(format.parse("2022-07-22"))+groupId)
                 val event1=Event(Color.GREEN,SimpleDateFormat("yyyy-mm-dd").parse("2022-07-22").time,"0")
 
 
                 calendarView.addEvent(event1)
 
-                it.result ?:return@addOnCompleteListener
-
+                snapshot.value ?:return
 
             }
-        }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"데이터를 가져오는데 실패했습니다",Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun getTodayWork(dateClicked:Date){
 
-        firebaseDatabase.child("group").child(groupId).child("log").child(getYear(dateClicked)).child(getMonth(dateClicked)).child(getDate(dateClicked)).get().addOnCompleteListener {
-            if(it.isSuccessful){
-                it.result ?: return@addOnCompleteListener
+        firebaseDatabase.child("group").child(groupId).child("log").child(getYear(dateClicked)).child(getMonth(dateClicked)).child(getDate(dateClicked)).addListenerForSingleValueEvent(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.value ==null) {
+                    initWorkList()
 
-                Log.i("info",getYear(dateClicked)+" "+getMonth(dateClicked)+" "+getDate(dateClicked))
-                Log.i("it",it.result.toString())
-                for(child in it.result.children){
-                    val workId=child.key.toString().toInt()
-                    Log.i("workId",workId.toString())
-                    val person= mutableListOf<Person>()
-
-                    for(child2 in child.child("manager").children){
-                        Log.i("child2",child2.toString())
-                        person.add(Person(child2.key.toString(),child2.child("userName").value.toString(),child2.child("isChecked").value.toString().toBoolean()))
-                    }
-
-                    if(workInfos[workId].number > person.size){ //3명 담당인데 1명만 되어있으면 2개를 추가
-                        for(i in 1..workInfos[workId].number -person.size){
-                            Log.i("not","in charged${workList[0].list[0].userName}")
-                            val list=workList[0].list[0].list.toMutableList()
-                            list.add(HomeThirdSection(false,workId,
-                                workInfos[workId].title))
-                            workList[0].list[0].list=list
+                }
+                else {
+                    Log.i("info",getYear(dateClicked)+" "+getMonth(dateClicked)+" "+getDate(dateClicked))
+                    Log.i("it",snapshot.value.toString())
+                    for(child in snapshot.children){
+                        val workId=child.key.toString().toInt()
+                        Log.i("workId",workId.toString())
+                        val person= mutableListOf<Person>()
+                        for(child2 in child.child("manager").children){
+                            Log.i("child2",child2.toString())
+                            person.add(Person(child2.child("userId").value.toString(),child2.child("userName").value.toString(),child2.child("isChecked").value.toString().toBoolean()))
                         }
 
-                    }
-
-                    Log.i("nmiddle",workList.toString())
-
-                    for(prs in person){
-                        Log.i("prs",prs.toString())
-
-                        for(element in workList[1].list){
-                            if(element.userId==prs.userId){
-                                val list=element.list.toMutableList()
-                                list.add(HomeThirdSection(prs.isChecked,workId,
+                        if(workInfos[workId].number > person.size){ //3명 담당인데 1명만 되어있으면 2개를 추가
+                            for(i in 1..workInfos[workId].number -person.size){
+                                Log.i("not","in charged${workList[0].list[0].userName}")
+                                val list=workList[0].list[0].list.toMutableList()
+                                list.add(HomeThirdSection(false,workId,
                                     workInfos[workId].title))
-                                element.list=list
+                                workList[0].list[0].list=list
                             }
+
                         }
 
+                        Log.i("nmiddle",workList.toString())
 
+                        for(prs in person){
+                            Log.i("prs",prs.toString())
+
+                            for(element in workList[1].list){
+                                if(element.userId==prs.userId){
+                                    val list=element.list.toMutableList()
+                                    list.add(HomeThirdSection(prs.isChecked,workId,
+                                        workInfos[workId].title))
+                                    element.list=list
+                                }
+                            }
+
+                        }
                     }
                 }
+
 
                 recyclerView.adapter= HomeFirstSectionRecyclerViewAdapter(workList)
                 recyclerView.layoutManager=LinearLayoutManager(requireContext())
             }
-        }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"데이터를 가져오는데 실패했습니다",Toast.LENGTH_SHORT).show()
+            }
+
+        })
 
     }
 
     private fun getWorkInfo() {
 
-        firebaseDatabase.child("group").child(groupId).child("worklist").get().addOnCompleteListener {
-            if(it.isSuccessful){
-                it.result ?:return@addOnCompleteListener
+        firebaseDatabase.child("group").child(groupId).child("worklist").addListenerForSingleValueEvent(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.value ?:return
 
-                it.result.children.forEach { dataSnapshot ->
+                snapshot.children.forEach { dataSnapshot ->
 
                     Log.i("success",dataSnapshot.toString())
                     dataSnapshot.getValue<Work>()?.let {it2->
@@ -178,18 +195,24 @@ class HomeFragment : Fragment() {
 
                 getUsers()
             }
-        }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"데이터를 가져오는데 실패했습니다",Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
         
     }
 
     private fun getUsers(){
         Log.i("getsuers","getUsers")
 
-        firebaseDatabase.child("group").child(groupId).child("users").get().addOnCompleteListener {
-            if(it.isSuccessful){
-                it.result?:return@addOnCompleteListener
+        firebaseDatabase.child("group").child(groupId).child("users").addListenerForSingleValueEvent(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.value?:return
 
-                it.result.children.forEach { dataSnapshot ->
+                snapshot.children.forEach { dataSnapshot ->
 
                     dataSnapshot.getValue<User>()?.let {it2->
                         val list=workList[1].list.toMutableList()
@@ -202,7 +225,12 @@ class HomeFragment : Fragment() {
 
                 init()
             }
-        }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),"데이터를 가져오는데 실패했습니다",Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     override fun onCreateView(
@@ -210,7 +238,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.i("fragment","createView")
         val instance=FirebaseDatabase.getInstance()
         firebaseDatabase=instance.reference
         return inflater.inflate(R.layout.fragment_home, container, false)
