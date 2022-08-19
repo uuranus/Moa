282c8a32d3e4877c560073c38e01b820cdd4806a
uuranus <72340294+uuranus@users.noreply.github.com>
2022-08-19 15:28:23 +0900

    Revert "Mypage"

--- app/src/main/java/com/moa/moa/Data/AlarmReceiver.kt
+++ app/src/main/java/com/moa/moa/Data/AlarmReceiver.kt
@@ -10,92 +10,45 @@ import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.edit
-import androidx.room.Room
-import com.google.firebase.database.FirebaseDatabase
-import com.moa.moa.Local.AlarmHistory
-import com.moa.moa.Local.AppDatabase
 import com.moa.moa.Main.HomeActivity
 import com.moa.moa.R
-import com.moa.moa.Utility
-import java.util.*
 
-class AlarmReceiver : BroadcastReceiver() {
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID = "1000"
-
+class AlarmReceiver :BroadcastReceiver(){
+    companion object{
+        const val NOTIFICATION_CHANNEL_ID="1000"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        val text = intent.getStringExtra("workTitle")
-        val id = intent.getIntExtra("workId", 0)
-        val groupId=intent.getStringExtra("groupId")
-        val userKey=intent.getStringExtra("userKey")
-
+        val text=intent.getStringExtra("workTitle")
+        val id=intent.getIntExtra("workId",0)
         createNotificationChannel(context)
 
-        val db = Room.databaseBuilder(
-            context,
-            AppDatabase::class.java,
-            "MoaDB"
-        ).fallbackToDestructiveMigration()
-            .build()
-
-        notifyNotification(context, id, text!!)
-
-        Thread {
-            db.alarmDao().insertHistory(AlarmHistory(null, text, Date(), 0))
-        }.start()
-
-        //첫 알림이면 뱃지 추가
-
-        val database=FirebaseDatabase.getInstance().reference
-
-        val sharedPreferences=context.getSharedPreferences("Info", Context.MODE_PRIVATE)
-
-        val alarms=sharedPreferences.getInt("alarm",0)
-
-        if(alarms==0){
-            database.child("group").child(groupId!!).child("users").child(userKey!!)
-                .child("badges").child("3").child("get").setValue(true)
-        }
-
-        sharedPreferences.edit(true){
-            putInt("alarm",alarms+1)
-        }
+        notifyNotification(context,id,text!!)
     }
 
-    private fun createNotificationChannel(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                "집안일 알림",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
+    private fun createNotificationChannel(context: Context){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val notificationChannel=NotificationChannel(NOTIFICATION_CHANNEL_ID,"집안일 알림",NotificationManager.IMPORTANCE_DEFAULT)
             NotificationManagerCompat.from(context).createNotificationChannel(notificationChannel)
         }
     }
 
-    private fun notifyNotification(context: Context, id: Int, text: String) {
-        val intent = Intent(context, HomeActivity::class.java) //알림 누르면 메인 홈 화면으로 이동
+    private fun notifyNotification(context:Context,id:Int,text:String){
+        val intent= Intent(context, HomeActivity::class.java) //알림 누르면 메인 홈 화면으로 이동
 
-        val pendingIntent =
-            PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent=
+            PendingIntent.getActivity(context,id,intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
 
-        with(NotificationManagerCompat.from(context)) {
-            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+        with(NotificationManagerCompat.from(context)){
+            val build=NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                 .setContentTitle("집안일 알람")
                 .setContentText("${text}을(를) 할 시간입니다")
                 .setSmallIcon(R.drawable.ic_baseline_notifications_24)
-                .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-            notify(id, build.build())
+            notify(id,build.build())
         }
-
-
-
     }
 
 }
--- app/src/main/java/com/moa/moa/Work/TitleDao.kt
+++ app/src/main/java/com/moa/moa/Work/TitleDao.kt
@@ -1,23 +1,21 @@
-package com.moa.moa.Local
+package com.moa.moa.Work
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.moa.moa.Data.TitleHistory
 
 @Dao
 interface TitleDao{
     @Query("SELECT * FROM titleHistory")
     fun getAll():List<TitleHistory>
 
-    @Query("SELECT * FROM titleHistory where title LIKE :title || '%' ")
+    @Query("SELECT * FROM titleHistory where title LIKE :title||'%' ")
     fun getHistory(title:String):List<TitleHistory>
 
     @Insert
-    fun insertHistory(history: TitleHistory)
+    fun insertHistory(history:TitleHistory)
 
     @Query("DELETE FROM titleHistory where title=:title")
     fun delete(title:String)
-
-    @Query("DELETE FROM titleHistory")
-    fun deleteAll()
 }
